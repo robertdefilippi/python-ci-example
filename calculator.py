3e0f6f644bc26c859e9262ba46dc16feab42c687
robertdefilippi <robert.defilippi@gmail.com>
2018-11-29 08:54:37 -0500

    First commit

---
+++ calculator.py
@@ -0,0 +1,34 @@
+"""
+Calcualtor libraru containing basic maths operations
+"""
+
+def add(first_term: int, second_term: int) -> int:
+    """
+    Add two integers together, and return the result.
+
+    Args:
+        first_term (int): The first term to add
+        second_term (int): The second term to add
+    
+    Return:
+        The addition result of first_term and second_term 
+    """
+
+    result = first_term + second_term
+    return result
+
+def subtract(first_term: int, second_term: int) -> int:
+    """
+    Subtract two integers together, and return the result.
+
+    Args:
+        first_term (int): The first term to add
+        second_term (int): The second term to add
+    
+    Return:
+        The subtraction result of first_term minus the second_term 
+    """
+    
+    result = first_term - second_term
+
+    return result
